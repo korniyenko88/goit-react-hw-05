43b13aeaf4233e7197f013c6c593bb845848dbde
Vladymyr <korniyenko88@gmail.com>
2024-10-25 00:23:54 +0300

    update movie list link

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './MovieList.module.css';
+import { Link } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
   return (
@@ -9,11 +10,14 @@ const MovieList = ({ movies }) => {
       ) : (
         movies.map(movie => (
           <div key={movie.id} className={styles.movieItem}>
-            <img
-              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-              alt={movie.title}
-              className={styles.moviePoster}
-            />
+            <Link to={`/movies/${movie.id}`} className={styles.link}> 
+              <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={movie.title}
+                className={styles.moviePoster}
+              />
+            </Link>
+            {/* Uncomment the following lines if you want to show the movie title and overview */}
             {/* <h3 className={styles.movieTitle}>{movie.title}</h3>
             <p className={styles.movieOverview}>{movie.overview}</p> */}
           </div>
